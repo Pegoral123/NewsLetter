c78223f8f95d5ad56b5e688ed971820b58391b17
Pegoral123 <jhonathanpegoral123@gmail.com>
2025-11-06 21:46:35 -0300

    Finalização da validação do ajax

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
   $("#form-newsLetter").submit(function (event) {
     //evita que formulario recarregue a pagina
     event.preventDefault();
+    console.log("Formulário interceptado!");
     //limpa as mensagens de feedback anterior
     $("#msg-sucesso").hide().text("");
     $("#msg-erro").hide().text("");
@@ -16,5 +17,32 @@ $(document).ready(function () {
         .show();
       return;
     }
+    $.ajax({
+      url: "cadastrar.php",
+      method: "POST",
+      data: {
+        nome: nome,
+        email: email,
+      },
+      dataType: "json",
+    })
+      .done(function (resposta) {
+        if (resposta.status === "sucesso") {
+          $("#msg-sucesso").text("Cadastro realizado com sucesso!").show();
+          $("#form-newsLetter")[0].reset();
+        } else {
+          $("#msg-erro")
+            .text("Erro ao cadastrar. Tente novamente mais tarde.")
+            .show();
+          console.log(resposta);
+        }
+      })
+      .fail(function () {
+        $("#msg-erro")
+          .text(
+            "Erro na comunicação com o servidor. Tente novamente mais tarde."
+          )
+          .show();
+      });
   });
 });
